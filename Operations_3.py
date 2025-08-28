bcf8ab5fccde62587fcf3ab80eda6cbb74c7debb
Saahir0711 <Saahir.dharamsi@gmail.com>
2025-08-28 11:36:26 +0100

    Add files via upload

---
+++ Operations_3.py
@@ -0,0 +1,17 @@
+outputFile = open("UpadatedFile.txt", "w")
+
+inputFile = open("Repeated.txt", "r")
+
+lines_seen_so_far = set()
+print("Elimintating duplicate lines...")
+
+for line in inputFile:
+
+    if line not in lines_seen_so_far:
+
+        outputFile.write(line)
+
+        lines_seen_so_far.add(line)
+
+inputFile.close()
+outputFile.close()
